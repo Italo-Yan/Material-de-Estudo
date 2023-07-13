094e630b136b9309b322e4524abee4a2a05d265d
italo <italoyan2004@gmai.com>
2023-07-13 19:17:36 -0300

    curso commit

--- aula88/aula88.js
+++ aula88/aula88.js
@@ -6,11 +6,71 @@ function rand(min, max) {
 
 function esperaAi(msg, tempo) {
     return new Promise((resolve, reject) => {
-        if(typeof msg !== 'string') reject(false);
+        if(typeof msg !== 'string') {
+        reject('ERROR');
+        return;
+    }
 
         setTimeout(() => {
-            resolve(msg);
+            resolve(msg.toUpperCase() + ' - Passei na promise');
         }, tempo);
     });
-
 }
+
+// Promise.all 
+/*
+const promises = [
+    'Primeiro valor',
+    esperaAi('Promise 1', 3000),
+    esperaAi('Promise 2', 500),
+    esperaAi('Promise 3', 1000),
+//  esperaAi(1000, 1000),
+    'Outro valor'
+];
+
+Promise.all(promises)
+    .then(function(valor) {
+        console.log(valor);
+    })
+    .catch(function(erro) {
+        console.log(erro);
+    });
+*/
+
+// Promise.race  
+/* 
+const promises = [
+    esperaAi(1000, rand(1, 5)),
+    esperaAi('Promise 1', rand(1, 5)),
+    esperaAi('Promise 2', rand(1, 5)),
+    esperaAi('Promise 3', rand(1, 5)),
+];
+
+Promise.race(promises)
+    .then(function(valor) {
+        console.log(valor);
+    })
+    .catch(function(erro) {
+        console.log(erro);
+    });
+*/
+
+// Promise.resolve || Promise.reject
+function baixaPagina() {
+    const emCache = true;
+
+    if(emCache) {
+        return Promise.resolve('Página em cache'); //.reject
+    } else {
+        return esperaAi('Baixe a página', 3000);
+    }
+}
+
+baixaPagina()
+    .then(dadosPagina => {
+        console.log(dadosPagina);
+    })
+    .catch( e => console.log(e));
+
+
+
